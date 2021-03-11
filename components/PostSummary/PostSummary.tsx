b6ac1cb51e2b9c25d6d76c73e84ca59ff5f3fe22
Pablo <porto.vga@gmail.com>
2021-03-11 12:11:55 +0100

    Fix accesibility errors

--- components/PostSummary/PostSummary.tsx
+++ components/PostSummary/PostSummary.tsx
@@ -15,11 +15,9 @@ export default function PostSummary(props: PostSummaryProps) {
                     <span className="font-light text-gray-600">
                         <Date dateString={date} />
                     </span>
-                    <a
-                        href="#"
-                        className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
+                    <span className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
                         {tag || 'Random'}
-                    </a>
+                    </span>
                 </div>
                 <div className="mt-3 text-2xl text-gray-700 font-bold">{title}</div>
                 <div className="flex justify-between items-center mt-4">
