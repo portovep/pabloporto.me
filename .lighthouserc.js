87da2c20f534ec0a908fa4e0e921d7cfd3902fbf
Pablo <porto.vga@gmail.com>
2021-03-12 16:21:30 +0100

    Change lighthouse assertion thresholds

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,8 +2,10 @@ module.exports = {
     ci: {
         assert: {
             assertions: {
-                'categories:performance': ['warn', { minScore: 0.9 }],
-                'categories:accessibility': ['error', { minScore: 1 }]
+                'categories:performance': ['error', { minScore: 0.9 }],
+                'categories:accessibility': ['warn', { minScore: 1 }],
+                'categories:best-practices': ['warn', { minScore: 1 }],
+                'categories:seo': ['warn', { minScore: 1 }]
             }
         }
     }
