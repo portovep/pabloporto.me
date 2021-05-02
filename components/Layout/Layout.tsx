4593e77d35cb2f2dfa3e4638cfa9745eaeadef74
Pablo <porto.vga@gmail.com>
2021-05-02 11:58:20 +0200

    Add additional page metatags
    Fix some issues wih metatags

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Footer from '../Footer/Footer';
 
 const name = 'Pablo Porto';
+const siteUrl = 'https://live.pabloporto.me/';
 export const siteTitle = 'Pablo Porto';
 const siteDescription =
     'Software developer, tech lead, and traveller. Currently helping companies go digital at ThoughtWorks.';
@@ -15,21 +16,29 @@ type LayoutProps = {
 };
 
 export default function Layout({ children, home, pageTitle }: LayoutProps): JSX.Element {
+    const title = pageTitle || siteTitle;
     return (
         <div className="max-w-4xl px-0 py-4 mx-auto mt-16">
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <title>{pageTitle}</title>
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content={pageTitle} />
-                <meta name="twitter:title" content={pageTitle} />
-                <meta name="description" content={pageTitle} />
+                <title>{title}</title>
+                <meta name="description" content={siteDescription} />
+                <meta property="og:title" content={title} />
                 <meta property="og:description" content={siteDescription} />
+                <meta property="og:image" content={`${siteUrl}/images/profile.jpg`} />
+                <meta property="og:image:alt" content="Pablo Porto"></meta>
+                <meta property="og:locale" content="en_US"></meta>
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={siteUrl}></meta>
+                <meta name="twitter:title" content={title} />
                 <meta name="twitter:description" content={siteDescription} />
-
-                <meta property="og:image" content="/images/profile.jpg" />
-                <meta name="twitter:image" content="/images/profile.jpg" />
+                <meta name="twitter:image" content={`${siteUrl}/images/profile.jpg`} />
+                <meta name="twitter:creator" content="@portovep"></meta>
+                <meta name="twitter:site" content="@portovep"></meta>
                 <meta name="twitter:card" content="summary_large_image" />
+
+                <meta name="robots" content="index,follow" />
+                <meta name="googlebot" content="index,follow" />
             </Head>
             <header className="flex flex-col items-center">
                 {home ? (
