b23ff98b16f19b7dd7afff9f9b445a2fbc16b47b
Pablo <porto.vga@gmail.com>
2021-05-18 22:13:30 +0200

    Fix photo grid in mobile view

--- components/PhotoCard/PhotoCard.tsx
+++ components/PhotoCard/PhotoCard.tsx
@@ -8,12 +8,11 @@ type PhotoCardProps = {
 
 export default function PhotoCard({ imageURL, location, title }: PhotoCardProps): JSX.Element {
     return (
-        <div className="overflow-hidden shadow-lg rounded-lg h-100 w-100 md:h-90 md:w-80 cursor-pointer m-auto">
-            <div className="h-96 w-96 relative">
+        <div className="overflow-hidden shadow-lg rounded-lg md:h-90 md:w-80 cursor-pointer m-auto">
+            <div className="h-96 w-72 sm:h-96 sm:w-96 relative">
                 <Image
                     alt={`${title} taken at ${location}`}
                     src={imageURL}
-                    className=""
                     layout="fill"
                     objectFit="cover"
                 />
