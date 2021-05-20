a23a4d1eb220d7918a866b863ac7d193eb649153
Pablo <porto.vga@gmail.com>
2021-05-20 22:56:31 +0200

    Add animate pin to draw attention to the header link

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -69,14 +69,22 @@ export default function Layout({
                         <>
                             <Link href="/about">
                                 <a>
-                                    <Image
-                                        priority
-                                        src="/images/profile.jpg"
-                                        className="rounded-full"
-                                        height={108}
-                                        width={108}
-                                        alt={name}
-                                    />
+                                    <div className="flex justify-around">
+                                        <span className="relative inline-flex rounded-md shadow-sm">
+                                            <Image
+                                                priority
+                                                src="/images/profile.jpg"
+                                                className="rounded-full"
+                                                height={108}
+                                                width={108}
+                                                alt={name}
+                                            />
+                                            <span className="flex absolute h-3 w-3 top-0 right-2 -mt-1 -mr-1">
+                                                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-300 opacity-75"></span>
+                                                <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-400"></span>
+                                            </span>
+                                        </span>
+                                    </div>
                                 </a>
                             </Link>
                             <h2 className="text-2xl leading-6 mt-4">
