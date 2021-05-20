fe9ea43801eb1e8388aba538d66aea989a1171fd
Pablo <porto.vga@gmail.com>
2021-05-20 22:43:30 +0200

    Add missing pages to performance check

--- scripts/performance-check.sh
+++ scripts/performance-check.sh
@@ -6,7 +6,9 @@ target_url=${1:-http://localhost}
 
 npx lhci autorun \
     --collect.numberOfRuns=3 \
+    --collect.url="${target_url}"/about \
     --collect.url="${target_url}"/posts \
+    --collect.url="${target_url}"/posts/what-is-like-in-my-current-project \
     --collect.url="${target_url}"/stack \
     --collect.url="${target_url}"/traveling \
     --collect.url="${target_url}"/working \
