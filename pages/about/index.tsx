8836f9d1d3bd78fb57d77dae39e4e4af4b5c4a12
Pablo <porto.vga@gmail.com>
2021-05-21 13:06:36 +0200

    Add additional skills in about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -176,6 +176,42 @@ export default function About(): JSX.Element {
                                     </span>
                                 </div>
                             </div>
+                            <div className="p-2 sm:w-1/2 w-full">
+                                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                                    <svg
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="3"
+                                        className="text-green-500 w-6 h-6 flex-shrink-0 mr-4"
+                                        viewBox="0 0 24 24">
+                                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                        <path d="M22 4L12 14.01l-3-3"></path>
+                                    </svg>
+                                    <span className="title-font font-medium">
+                                        Trunk based development
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="p-2 sm:w-1/2 w-full">
+                                <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+                                    <svg
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="3"
+                                        className="text-green-500 w-6 h-6 flex-shrink-0 mr-4"
+                                        viewBox="0 0 24 24">
+                                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                        <path d="M22 4L12 14.01l-3-3"></path>
+                                    </svg>
+                                    <span className="title-font font-medium">
+                                        Continuous improvement
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
