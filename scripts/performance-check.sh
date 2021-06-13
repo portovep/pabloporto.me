cafb4a7293827186d1daca853d1bddb606597d82
Pablo <porto.vga@gmail.com>
2021-06-13 13:57:23 +0200

    Fix performance check url

--- scripts/performance-check.sh
+++ scripts/performance-check.sh
@@ -8,7 +8,7 @@ npx lhci autorun \
     --collect.numberOfRuns=3 \
     --collect.url="${target_url}"/about \
     --collect.url="${target_url}"/blog \
-    --collect.url="${target_url}"/blog/what-is-like-in-my-current-project \
+    --collect.url="${target_url}"/blog/what-is-like-to-be-a-developer-at-thoughtworks \
     --collect.url="${target_url}"/stack \
     --collect.url="${target_url}"/traveling \
     --collect.url="${target_url}"/working \
