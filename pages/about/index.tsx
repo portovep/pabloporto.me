e8c340525e75bc17ab51d9e52224da6fd97e116a
Pablo <porto.vga@gmail.com>
2021-08-31 09:17:00 +0200

    Add skills to about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -85,7 +85,7 @@ export default function About(): JSX.Element {
                                         <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                                         <path d="M22 4L12 14.01l-3-3"></path>
                                     </svg>
-                                    <span className="title-font font-medium">CI/CD</span>
+                                    <span className="title-font font-medium">Continuous delivery</span>
                                 </div>
                             </div>
                             <div className="sm:w-1/2 w-full p-2">
@@ -244,6 +244,22 @@ export default function About(): JSX.Element {
                                         <path d="M22 4L12 14.01l-3-3"></path>
                                     </svg>
                                     <span className="title-font font-medium">Clean code</span>
+                                </div>
+                            </div>
+                            <div className="sm:w-1/2 w-full p-2">
+                                <div className="flex items-center h-full p-4 bg-gray-100 rounded">
+                                    <svg
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="3"
+                                        className="flex-shrink-0 w-6 h-6 mr-4 text-green-500"
+                                        viewBox="0 0 24 24">
+                                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                        <path d="M22 4L12 14.01l-3-3"></path>
+                                    </svg>
+                                    <span className="title-font font-medium">Refactoring</span>
                                 </div>
                             </div>
                         </div>
