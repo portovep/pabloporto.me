423e731a3288e90afb7d6255baf438b89d8a3563
Pablo <porto.vga@gmail.com>
2021-12-23 08:59:05 +0100

    Fix issue importing new version of remark

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import remark from 'remark';
+import { remark } from 'remark';
 import html from 'remark-html';
 
 const projectDirectory = path.join(process.cwd(), 'content/projects');
