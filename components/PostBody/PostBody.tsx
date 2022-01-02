e3f928554d6a66795f7dea30ea9b8ca66b649edd
Pablo <porto.vga@gmail.com>
2022-01-02 14:27:23 +0100

    Upgrade to Tailwindcss >3.0

--- components/PostBody/PostBody.tsx
+++ components/PostBody/PostBody.tsx
@@ -5,7 +5,7 @@ type PostBodyProps = {
 export default function PostBody(props: PostBodyProps): JSX.Element {
     return (
         <div
-            className="lg:prose-xl mt-10 prose"
+            className="lg:prose-xl prose-a:text-emerald-500 hover:prose-a:text-emerald-600 hover:prose-a:font-medium prose-a:no-underline mt-10 prose"
             dangerouslySetInnerHTML={{ __html: props.contentHtml }}
         />
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,41 +2,20 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     theme: {
         colors: {
             transparent: 'transparent',
             current: 'currentColor',
             black: colors.black,
             white: colors.white,
-            gray: colors.coolGray,
+            gray: colors.gray,
             indigo: colors.indigo,
             lime: colors.lime,
             green: colors.green,
             emerald: colors.emerald,
             red: colors.red
-        },
-        extend: {
-            typography: (theme) => ({
-                DEFAULT: {
-                    css: {
-                        color: '#333',
-                        a: {
-                            color: theme('colors.emerald.500'),
-                            '&:hover': {
-                                color: theme('colors.emerald.600'),
-                                fontWeight: '500'
-                            },
-                            textDecoration: 'none'
-                        }
-                    }
-                }
-            })
         }
-    },
-    variants: {
-        extend: {}
     },
     plugins: [require('@tailwindcss/typography')]
 };
