d8a0fda0da82259eee9d7a2ca5cbaa195e0a15be
Pablo <porto.vga@gmail.com>
2022-08-18 17:45:33 +0200

    Decrease performance threshold to accommodate for map SVG

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
         assert: {
             assertions: {
-                'categories:performance': ['error', { minScore: 0.9 }],
+                'categories:performance': ['error', { minScore: 0.85 }],
                 'categories:accessibility': ['error', { minScore: 0.9 }],
                 'categories:best-practices': ['error', { minScore: 0.9 }],
                 'categories:seo': ['error', { minScore: 0.9 }]
