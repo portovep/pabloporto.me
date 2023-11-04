fb287511f9e7763ba9b56ece328ec45006f4ce3f
Pablo <porto.vga@gmail.com>
2023-11-04 19:18:08 +0100

    Fix formatting issues

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     webpack(config) {
         config.module.rules.push({
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: ['@svgr/webpack'],
-        })
-    
-        return config
-      },
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack']
+        });
+
+        return config;
+    },
     eslint: {
         dirs: ['pages', 'components', 'lib', 'src', 'cypress']
     }
