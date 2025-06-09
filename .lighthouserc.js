82f4750687b3fab85433fe7a99ff06b772f7ce9f
Pablo <porto.vga@gmail.com>
2025-06-09 21:12:03 +0200

    Add pre-commit hooks with husky

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
                 'categories:performance': ['error', { minScore: 0.6 }],
                 'categories:accessibility': ['error', { minScore: 0.9 }],
                 'categories:best-practices': ['error', { minScore: 0.9 }],
-                'categories:seo': ['error', { minScore: 0.66 }],
+                'categories:seo': ['error', { minScore: 0.66 }]
             }
         },
         upload: {
